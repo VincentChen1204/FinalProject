514e4d3169a2496438810ee43ed909b6e6866752
VincentChen <vincentc273@nycstudents.net>
2026-06-05 13:17:01 -0400

    pt2

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -12,10 +12,31 @@ import java.io.IOException;
 public class DisplayPanel extends JPanel implements MouseListener, KeyListener {
     private int score;
     private BufferedImage background;
-    private BufferedImage ball;
+    private int ballSpawnX;
+    private int ballSpawnY;
     public DisplayPanel() {
-
+        score = 0;
+        try {
+            background = ImageIO.read(new File("Background.webp"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        ballSpawnX = (int) (Math.random() * 500) + 50;
+        ballSpawnY = (int) (Math.random() * 400) + 50;
+        addMouseListener(this);
+        addKeyListener(this);
+        setFocusable(true);
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (background != null) {
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        }
+        g.setColor(java.awt.Color.RED);
+        g.fillOval(ballSpawnX, ballSpawnY, 50, 50);
+    }
+
     public void mouseClicked(MouseEvent e) {
 
     }
@@ -27,7 +48,12 @@ public class DisplayPanel extends JPanel implements MouseListener, KeyListener {
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            Point clickLocation = e.getPoint();
+            int x = clickLocation.x;
+            int y = clickLocation.y;
+            repaint();
+        }
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Aim Trainer");
+        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(960, 580);
         frame.setLocationRelativeTo(null);
         DisplayPanel panel = new DisplayPanel();
         frame.add(panel);
